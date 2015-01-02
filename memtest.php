14839e6e083db82e4987a016d4f8a1c7a1fdb872
Aaron Jorbin <aaron@jorb.in>
2015-01-01 22:58:28 -0500

    refactor into a class

--- memtest.php
+++ memtest.php
@@ -1,139 +1,188 @@
 <?php
 
-$results = array();
-$times = 10000;
-$m = new Memcache();
-$m->addServer( '127.0.0.1', 11211 );
-$d = new Memcached();
-$d->addServer( '127.0.0.1', 11211 );
-$b = new Memcached();
-$b->addServer( '127.0.0.1', 11211 );
-$b->setOption( Memcached::OPT_BINARY_PROTOCOL, true );
+class memTest{
 
-function randomString($length = 10) {
-	return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+  private $_connections = array();
+  private $_connection_types = array();
+  private $times = 1000000;
+  private $results = array();
+  private $keys = array();
+
+  function __construct() {
+    $this->_connections['memcache'] = new Memcache();
+    $this->_connections['memcached'] = new Memcached();
+    $this->_connections['memcached_withBinaryProtocol'] = new Memcached();
+
+    foreach( $this->_connections as $connection) {
+      $connection->addServer( '127.0.0.1', 11211 );
+
+    }
+    $this->_connections['memcached_withBinaryProtocol']->setOption( Memcached::OPT_BINARY_PROTOCOL, true );
+
+    $this->_connection_types = array_keys( $this->_connections );
+
+    $this->createKeys();
+
+  }
+
+  /**
+   * Populates $this->keys with random values for each connection
+   */
+  private function createKeys() {
+    foreach ( $this->_connection_types as $connection ) {
+      $this->keys[ $connection ] = array();
+    }
+
+    for ( $t = 0; $t < $this->times; $t++ ) {
+      $key = $this->_randomString(); 
+      $value = $this->_randomString( rand( 1, 2000 ) );
+      foreach ( $this->_connection_types as $connection ) {
+        $this->keys[ $connection ][] = array( 
+          'key' => $key . $connection,
+          'value' => $value
+        );
+      }
+    }
+  }
+
+  /**
+   * Test the add method
+   */
+  function testAdds() {
+    foreach( $this->_connection_types as $ct ){
+      for ( $t = 0; $t < $this->times; $t++ ) {
+        $memcacheKey = $this->keys[ $ct ][ $t ][ 'key' ]; 
+        $value = $this->keys[ $ct ][ $t ][ 'value' ]; 
+        $start = microtime(true);
+
+        $this->_connections[ $ct ]->add( 'add-' . $memcacheKey , $value ); 
+
+        $end = microtime( true );
+        $execution = $end - $start;
+
+        $this->results[ 'add' ][ $ct ][] = $execution;
+      }
+    }
+  }
+
+  /**
+   * Test the set method
+   */
+  function testSets() {
+    foreach( $this->_connection_types as $ct ){
+      for ( $t = 0; $t < $this->times; $t++ ) {
+        $memcacheKey = $this->keys[ $ct ][ $t ][ 'key' ]; 
+        $value = $this->keys[ $ct ][ $t ][ 'value' ]; 
+        $start = microtime(true);
+
+        $this->_connections[ $ct ]->set( 'set-' . $memcacheKey , $value ); 
+
+        $end = microtime( true );
+        $execution = $end - $start;
+
+        $this->results[ 'set' ][ $ct ][] = $execution;
+      }
+    }
+  }
+
+  /**
+   * Test the get method
+  */
+  function testGets() {
+    foreach( $this->_connection_types as $ct ){
+      for ( $t = 0; $t < $this->times; $t++ ) {
+        $memcacheKey = $this->keys[ $ct ][ $t ][ 'key' ]; 
+        $value = $this->keys[ $ct ][ $t ][ 'value' ]; 
+        $start = microtime(true);
+
+        $this->_connections[ $ct ]->get( 'add-' . $memcacheKey ); 
+        $this->_connections[ $ct ]->get( 'set-' . $memcacheKey ); 
+
+        $end = microtime( true );
+        $execution = $end - $start;
+
+        $this->results[ 'get' ][ $ct ][] = $execution;
+      }
+    }
+  }
+
+  /**
+   * Echo out the results
+   */
+  function reportResults() {
+
+    // We want padded names for display
+    $lengths = array_map( 'strlen', $this->_connection_types );
+    $longest = max( $lengths ) + 2; 
+
+    echo "\nResults are in thousandth of a second";
+    foreach( $this->results as $k => $tests ) {
+      echo "\n---";
+      echo "\nTEST: $k";
+
+     
+      echo "\n\tAverage Execution:";
+      foreach( $this->_connection_types as $ct ){
+        $name = str_pad( $ct, $longest );
+        echo "\n$name: " . $this->_formatNum( array_sum( $tests[$ct] ) / count( $tests[$ct] ) );
+      }
+
+      echo "\n\t95% execution time";
+      foreach( $this->_connection_types as $ct ){
+        $name = str_pad( $ct, $longest );
+        $ninety_five = round( ( $this->times / 100 ) * 95 );
+
+        sort( $tests[$ct] );
+
+        echo "\n$name: " . $this->_formatNum( $tests[$ct][ $ninety_five ] );
+      }
+
+      echo "\n\t99% execution time";
+      foreach( $this->_connection_types as $ct ){
+        $name = str_pad( $ct, $longest );
+        $ninety_nine = round( ( $this->times / 100 ) * 99 );
+
+        sort( $tests[$ct] );
+
+        echo "\n$name: " . $this->_formatNum( $tests[$ct][ $ninety_nine ] );
+      }
+
+      echo "\n";
+    }
+    
+
+  }
+
+  /**
+   * Return a random string
+   *
+   * @param int $length Length of string 
+  */
+  function _randomString($length = 10) {
+    $str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    while( strlen( $str) < $length ) {
+      $str .= $str;
+    }
+    return substr( str_shuffle( $str ), 0, $length );
+  }
+
+  /**
+   * Return a number in our desired format
+   *
+   * Since the results are generally in ten-thousands, multiply results by 1000 and
+   * Only return the first four places after the decimal
+   *
+   * @parm float $number the number we want formated
+  */
+  function _formatNum( $number ) {
+    return number_format( $number * 1000, 4 );
+  }
+
+
 }
 
-function formatNum( $number ) {
-	return number_format( $number * 1000, 4 );
-}
-
-
-for ( $t = 0; $t < $times; $t++ ) {
-
-	$key = randomString(); 
-	$memcacheKey = $key . 'm';
-	$memcachedKey = $key . 'd';
-	$memcachedbKey = $key . 'b';
-	$value = randomString( rand( 1, 200 ) );
-
-
-	// memcache add
-	$start = microtime(true);
-
-	$m->add( 'add-' . $memcacheKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'add' ][ 'memcache' ][] = $execution;
-
-	// memcached add
-	$start = microtime(true);
-
-	$d->add( 'add-' . $memcachedKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'add' ][ 'memcached' ][] = $execution;
-
-	// memcached binary add
-	$start = microtime(true);
-
-	$b->add( 'add-' . $memcachedKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'add' ][ 'memcachedBinary' ][] = $execution;
-
-	// memcache set
-	$start = microtime(true);
-
-	$m->set( 'set-' . $memcacheKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'set' ][ 'memcache' ][] = $execution;
-
-	// memcached set
-	$start = microtime(true);
-
-	$d->set( 'set-' . $memcachedKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'set' ][ 'memcached' ][] = $execution;
-
-	// memcached set
-	$start = microtime(true);
-
-	$b->set( 'set-' . $memcachedKey , $value );
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'set' ][ 'memcachedBinary' ][] = $execution;
-
-	// memcache get
-	$start = microtime(true);
-
-	$m->get( 'add-' . $memcacheKey ); 
-	$m->get( 'set-' . $memcacheKey ); 
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'get' ][ 'memcache' ][] = $execution;
-
-	// memcached get
-	$start = microtime(true);
-
-	$d->get( 'add-' . $memcacheKey ); 
-	$d->get( 'set-' . $memcacheKey ); 
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'get' ][ 'memcached' ][] = $execution;
-
-	// memcached get
-	$start = microtime(true);
-
-	$b->get( 'add-' . $memcacheKey ); 
-	$b->get( 'set-' . $memcacheKey ); 
-
-	$end = microtime( true );
-	$execution = $end - $start;
-
-	$results[ 'get' ][ 'memcachedBinary' ][] = $execution;
-
-}
-
-echo "\nResults are in thousandth of a second";
-foreach( $results as $k => $tests ) {
-	echo "\n---";
-	echo "\nTEST: $k";
-	foreach( $tests as $type => $v) { 
-		echo "\n$type: Average Execution: " . formatNum( array_sum( $v ) / count( $v ) );
-		sort( $v );
-		$ninety_nine = round( ( $times / 100 ) * 99 );
-		$ninety_five = round( ( $times / 100 ) * 95 );
-		echo "\n$type: 95%: " . formatNum( $v[ $ninety_five ] );
-		echo "\n$type: 99%: " . formatNum( $v[ $ninety_nine ] );
-	}
-	echo "\n";
-}
+$m = new MemTest();
+$m->testAdds();
+$m->testSets();
+$m->testGets();
+$m->reportResults();
